f709b3959d778a9e8c3d82ed08af9e59869b8cea
majicyu <qaz13397035986@gmail.com>
2021-08-11 11:56:28 +0800

    sql_update

--- sql_main.py
+++ sql_main.py
@@ -9,20 +9,12 @@ connection = pymysql.connect(host='localhost',
 
 cursor = connection.cursor()
 
-sql = "SELECT * FROM words where incorrect> %d" %(-1)
+sql = "UPDATE words set correct = correct+1 where incorrect = %d" %(0)
 try:
     cursor.execute(sql)
-    results = cursor.fetchall()
-    for row in results:
-        english = row[1]
-        chinese = row[2]
-        correct = row[3]
-        incorrect = row[4]
-        print("english=%s,chinese=%s,correct=%d,incorrect=%d" %(english,chinese,correct,incorrect))
+    connection.commit()
 except:
-    import traceback
-    traceback.print_exc()
-    print("ERROR:unable to fetch data")
+    connection.rollback()
 #print(sql)
-print("YES,Select Successful.")
+print("YES,UPDATE Successful.")
 connection.close()
